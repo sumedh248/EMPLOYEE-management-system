40fcc84cc1defd74a7e26c2693e13b17d872f478
sumedh248 <160852535+sumedh248@users.noreply.github.com>
2025-03-23 23:44:52 +0530

    remained filess

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+.style2 {
+	font-size: 14px;
+	color: #000000;
+}
+-->
+</style>
+ <div class="footer">
+          <div class="style1"> <p align="center" class="style2"> </p>
+</div>
+</div>
