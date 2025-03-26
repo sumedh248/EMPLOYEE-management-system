214b4957879717e1f442b98e2b3291a6ec44fc13
sumedh248 <160852535+sumedh248@users.noreply.github.com>
2025-03-26 23:13:33 +0530

    Update employee_akpoly.sql

--- employee_akpoly.sql
+++ employee_akpoly.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2025 at 07:49 PM
+-- Generation Time: Mar 26, 2025 at 04:16 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -46,16 +46,18 @@ CREATE TABLE `tblemployee` (
   `status` varchar(1) NOT NULL,
   `leave_status` varchar(20) NOT NULL,
   `photo` varchar(5000) NOT NULL,
-  `joiningdate` varchar(255) NOT NULL
+  `joiningdate` varchar(255) NOT NULL,
+  `leaves` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblemployee`
 --
 
-INSERT INTO `tblemployee` (`id`, `employeeID`, `fullname`, `password`, `sex`, `email`, `dob`, `phone`, `address`, `qualification`, `dept`, `employee_type`, `date_appointment`, `basic_salary`, `gross_pay`, `status`, `leave_status`, `photo`, `joiningdate`) VALUES
-(9, 'STAFF/FKP/2025/1581', 'ram misra', 'Ram@1234', 'Male', 'main12@gmail.com', '24/8/2002', '3636363636', 'KM', 'GRADUATEED', 'Security', 'Academic', '2/2/2000', '2000000', '30000', '1', 'Not Available', 'uploadImage/Profile/WhatsApp Image 2024-11-26 at 15.01.13_ba0d946f.jpg', '2025-03-24'),
-(11, 'STAFF/FKP/2025/1737', 'mayur', 'Mayur123', 'Male', 'main132@gmail.com', '', '03636363636', '', '', 'Student Affairs', 'Academic', '', '300000000', '30000000', '', 'Not Available', '', '2025-03-24');
+INSERT INTO `tblemployee` (`id`, `employeeID`, `fullname`, `password`, `sex`, `email`, `dob`, `phone`, `address`, `qualification`, `dept`, `employee_type`, `date_appointment`, `basic_salary`, `gross_pay`, `status`, `leave_status`, `photo`, `joiningdate`, `leaves`) VALUES
+(9, 'STAFF/FKP/2025/1581', 'ram misra', 'Ram@1234', 'Male', 'main12@gmail.com', '24/8/2002', '3636363636', 'KM', 'GRADUATEED', 'Security', 'Academic', '2/2/2000', '2000000', '30000', '1', 'Not Available', 'uploadImage/Profile/WhatsApp Image 2024-11-26 at 15.01.13_ba0d946f.jpg', '2025-03-24', 8),
+(11, 'STAFF/FKP/2025/1737', 'mayur', 'Mayur123', 'Male', 'main132@gmail.com', '', '03636363636', '', '', 'Student Affairs', 'Academic', '', '300000000', '30000000', '', 'Not Available', '', '2025-03-24', 0),
+(12, 'STAFF/FKP/2025/3381', 'ram misraaa', 'Ramm1234', 'Male', 'main1112@gmail.com', '', '03636363636', '', '', 'Bursary', 'Academic', '', '30000000000', '30000000000', '', 'Not Available', '', '2025-03-27', 8);
 
 -- --------------------------------------------------------
 
@@ -79,7 +81,13 @@ CREATE TABLE `tblleave` (
 
 INSERT INTO `tblleave` (`ID`, `email`, `leaveID`, `start_date`, `end_date`, `reason`, `status`) VALUES
 (20, 'main12@gmail.com', '2025103', '2025-02-24', '2025-02-25', 'Sick Leave', 'Declined'),
-(21, 'main12@gmail.com', '2025503', '2025-02-24', '2025-02-25', 'Sick Leave', 'Approved');
+(21, 'main12@gmail.com', '2025503', '2025-02-24', '2025-02-25', 'Sick Leave', 'Approved'),
+(22, 'main132@gmail.com', '2025362', '2025-03-26', '2025-03-29', 'Study Leave', 'Approved'),
+(23, 'main132@gmail.com', '2025172', '2025-03-26', '2025-03-29', 'Study Leave', 'Approved'),
+(24, 'main132@gmail.com', '2025498', '2025-03-26', '2025-03-29', 'Sick Leave', 'Approved'),
+(25, 'main132@gmail.com', '2025483', '2025-03-26', '2025-03-29', 'Monthly Leave', 'Approved'),
+(26, 'main132@gmail.com', '2025371', '2025-03-28', '2025-03-31', 'Sick Leave', 'Approved'),
+(27, 'main132@gmail.com', '2025337', '2025-04-01', '2025-04-05', 'Study Leave', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -133,13 +141,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tblemployee`
 --
 ALTER TABLE `tblemployee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tblleave`
 --
 ALTER TABLE `tblleave`
-  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
